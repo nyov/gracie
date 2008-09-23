cf096c7cd5caef1ef58094a7cefa13d11071a22b
Ben Finney <ben@benfinney.id.au>
2008-09-23 13:46:21 +1000

    Add makefile rules to build manpages from reST.

--- doc/module.mk
+++ doc/module.mk
@@ -38,6 +38,8 @@ GENERATED_FILES += ${manpage_files}
 RST2HTML = rst2html
 RST2HTML_OPTS =
 
+PANDOC = pandoc
+
 CONVERT = convert
 
 
@@ -55,6 +57,12 @@ man: ${manpage_files}
 
 %.8: %.8.sgml
 	docbook-to-man $< > $@
+
+%.1: %.1${RST_SUFFIX}
+	$(PANDOC) --from rst --to man $< > $@
+
+%.8: %.8${RST_SUFFIX}
+	$(PANDOC) --from rst --to man $< > $@
 
 
 .PHONY: xhtml
